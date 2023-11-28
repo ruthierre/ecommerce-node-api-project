3374c937deab707d8e36b85d322826c511f9dbeb
Walber Ruthierre da Costa <ruthierrex@gmail.com>
2023-11-27 21:43:28 -0300

    added router `api.v1.ts`

--- src/main/presentation/http/server.ts
+++ src/main/presentation/http/server.ts
@@ -1,14 +1,19 @@
 import http from 'node:http';
+import express, { Application } from 'express';
+import { apiv1Router } from './rest/api.v1';
+import morgan from 'morgan';
+
+const app: Application = express();
 
 const createHTTPServer = async (): Promise<http.Server> => {
     
-    const httpServer: http.Server = http.createServer(
-        function(request, response){
-            response.writeHead(200, {'Contet-Type': 'text/html'});
-            response.write('<html><body><p>Hello, im server node.js!</p></body></html>');
-            response.end();
-        }
-    );
+    app.disabled('x-powered-by');
+    app.use(express.json());
+    app.use(morgan('tiny'));
+
+    app.use('/api/v1', apiv1Router);
+
+    const httpServer: http.Server = http.createServer(app);
 
     return httpServer;
 }
