0e3d6bcf21d4cb8aca48af0080ddd011d4cae20c
Walber Ruthierre da Costa <ruthierrex@gmail.com>
2024-01-16 16:25:36 -0300

    added tests to domain `Usuario`

--- src/modules/usuario/domain/usuario.spec.ts
+++ src/modules/usuario/domain/usuario.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker';
 import { beforeAll, describe, expect, test } from "vitest";
 import { CriarUsuarioProps, TipoUsuario } from './usuario.types';
 import { Usuario } from './usuario.entity';
+import { UsuarioExceptions } from './usuario.exception';
 
 let nomeUsuarioValido : string;
 let nomeUsuarioTamanhoMinimoInvalido: string;
@@ -16,11 +17,11 @@ beforeAll(async ()=> {
     nomeUsuarioTamanhoMinimoInvalido = faker.string.alpha({length:{min:0,max:2}});
     nomeUsuarioTamanhoMaximoInvalido = faker.string.alpha({length:{min:51,max:51}});
 
-    emailValido = faker.internet.email();
+    emailValido = faker.internet.exampleEmail();
     emailInvalido = faker.string.alpha();
     
     senhaValida = faker.internet.password();
-    tipoValido = TipoUsuario.CLIENTE;
+    tipoValido = faker.helpers.enumValue(TipoUsuario)
     
 })
 
@@ -36,6 +37,42 @@ describe('Entidade de Domínio: Usuário', ()=> {
 
             expect(Usuario.criar(usuarioValido))
                 .to.be.instanceOf(Usuario);
-        })
-    })
+        });
+
+        test('Não deve criar um usuário com nome minimo inválido',async () => {
+            const usuarioNomeMinimoInvalido: CriarUsuarioProps = {
+                nome: nomeUsuarioTamanhoMinimoInvalido,
+                email: emailValido,
+                senha: senhaValida,
+                tipo: tipoValido
+            }
+
+            expect(() => Usuario.criar(usuarioNomeMinimoInvalido))
+                .toThrowError(UsuarioExceptions.NomeUsuarioTamanhoMinimoInvalido);
+        });
+
+        test('Não deve criar um usuario com nome máximo Inválido',async () => {
+            const usuarioNomeMaximoInvalido: CriarUsuarioProps = {
+                nome: nomeUsuarioTamanhoMaximoInvalido,
+                email: emailValido,
+                senha: senhaValida,
+                tipo: tipoValido
+            }
+
+            expect(()=> Usuario.criar(usuarioNomeMaximoInvalido))
+                .toThrowError(UsuarioExceptions.NomeUsuarioTamanhoMaximoInvalido);
+        });
+
+        test('Não deve criar um usuário com email inválido',async () => {
+            const usuarioEmailInvalido: CriarUsuarioProps = {
+                nome:nomeUsuarioValido,
+                email: emailInvalido,
+                senha: senhaValida,
+                tipo: tipoValido
+            }
+
+            expect(()=> Usuario.criar(usuarioEmailInvalido))
+                .toThrowError(UsuarioExceptions.EmailInvalido);
+        });
+    });
 })
